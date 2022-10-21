4578adbe44fa36bf2043a134c507ecb0f6f33963
DaniloSlavnic <34534405+DaniloSlavnic@users.noreply.github.com>
2022-10-20 22:07:59 -0700

    Update Queue.h

--- Queue/Queue.h
+++ Queue/Queue.h
@@ -1,9 +1,35 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
-template <typename T>
+template<typename T>
 class Queue{
-  
-}
+public:
+    Queue();
+    Queue(const Queue&);
+    Queue(Queue&&);
+    ~Queue();
 
-#endif
+    T& pop();
+    void push(const T&);
+    void print();
+
+private:
+    void resize(int);
+    int size;
+    T * head;
+};
+
+template<typename T>
+class PriorityQueue : Queue{
+    PriorityQueue();
+    PriorityQueue(const PriorityQueue&);
+    PriorityQueue(PriorityQueue&&);
+    ~PriorityQueue();
+
+    T& pop();
+    void push(int);
+private:
+    const T**;
+};
+
+#endif 
