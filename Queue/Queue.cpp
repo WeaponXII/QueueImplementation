b92d0c7617577012c738a6810e6e80df262c6016
WeaponXII <29986707+WeaponXII@users.noreply.github.com>
2022-10-24 18:07:19 -0700

    Minor additions to demo file

--- Queue/Queue.cpp
+++ Queue/Queue.cpp
@@ -7,11 +7,18 @@ int main() {
 	cout << "Running file." << endl;
 	PriorityQueue<int> example;
 	Queue<int> nums;
+	Queue<int> copyNums;
 	try {
 		for (int i = 0; i < 10; i++) {
 			nums.push(i);
 		}
 		cout << nums << endl;
+		cout << "Is nums empty? " << nums.isEmpty() << endl;
+		cout << "Is copyNums empty? " << copyNums.isEmpty() << endl;
+		copyNums(nums);
+		copyNums.pop();
+		cout << copyNums << endl;
+		example.push(5);
 	}
 	catch(const std::exception& e) {
 		cout << endl <<"ERROR: " << e.what() << endl;
